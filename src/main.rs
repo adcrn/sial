94f2292ae2a85923e23d11a8704d0c16160e9ed1
Alexander deCurnou <adecurnou@gmail.com>
2016-03-13 22:43:09 -0400

    Added args code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+#![allow(dead_code)]
+
 use std::io;
 use std::env;
+use std::fs::File;
+use std::error::Error;
+use std::path::Path;
 
 struct MeanFeatures {
-    wordlen: i32,	// The length of each of these
-    sentlen: i32,	// characteristics will be averaged
-    paralen: i32,	// and compared with other source.
+    wordlen: i32,	// The length of each of these characteristics will be
+    sentlen: i32,	// averaged and compared with other source.
+    //paralen: i32,	// Removing this for now.
 }
 
 struct FreqFeatures {
@@ -16,7 +21,7 @@ struct FreqFeatures {
     ands: i32,
     buts: i32,
     however: i32,
-    condition: i32,		// Did not want to create confusion with if.
+    condition: i32,		// Prevent confusion with if.
     thats: i32,
     more: i32,
     musts: i32,
@@ -27,4 +32,12 @@ struct FreqFeatures {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let file1 = match File::open(&args[1]) {
+        Err(why) => panic!("Couldn't open {}: {}", &args[1], Error::description(&why)),
+        Ok(file1) => file1,
+    };
+    let file2 = match File::open(&args[2]) {
+        Err(why) => panic!("Couldn't open {}: {}", &args[2], Error::description(&why)),
+        Ok(file2) => file2,
+    };
 }
