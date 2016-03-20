444352f8de43460ca8fd227489f340b9e05b2ce9
Alexander deCurnou <adecurnou@gmail.com>
2016-03-20 02:41:32 -0400

    Started struct function design

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ use std::env;
 use std::fs::File;
 use std::error::Error;
 use std::default::Default;
-use std::char;
 
 mod tokenizer;      // Tokenizes files into words, sentences, and paragraphs.
-mod calculator;     // Does all the 
+mod calculator;     // Does all the calculations for stats
 
 #[derive(Default, Copy, Clone)]
 struct Metadata {
@@ -31,6 +30,19 @@ struct Metadata {
     mights: i32,
     thises: i32,
     very: i32,
+}
+
+impl Metadata {
+    fn words(&mut self, s: Vec<&str>) {
+        self.word_total = calculator::num_words(&s);
+        self.wordlen = calculator::word_mean(&s);
+        println!("Mean word length: {}", self.wordlen);
+    }
+
+    fn sents(&mut self, s: Vec<&str>) {
+        self.sentlen = calculator::sent_mean(&self.word_total, &s);
+        println!("Mean sentence length: {}", self.sentlen);
+    }
 }
 
 fn main() {
@@ -63,18 +75,11 @@ fn main() {
 
     let s1_token: Vec<&str> = tokenizer::word_token(&s1);
     let s2_token: Vec<&str> = tokenizer::word_token(&s2);
-    off.word_total = calculator::num_words(&s1_token);
-    pseu.word_total = calculator::num_words(&s2_token);
-    off.wordlen = calculator::word_mean(&s1_token);
-    pseu.wordlen = calculator::word_mean(&s2_token);
-    println!("Mean word length #1: {}", off.wordlen);
-    println!("Mean word length #2: {}", pseu.wordlen);
-
     let s1_sent: Vec<&str> = tokenizer::sent_token(&s1);
     let s2_sent: Vec<&str> = tokenizer::sent_token(&s2);
-    off.sentlen = calculator::sent_mean(&off.word_total, &s1_sent);
-    pseu.sentlen = calculator::sent_mean(&pseu.word_total, &s2_sent);
-    println!("Mean sentence length #1: {}", off.sentlen);
-    println!("Mean sentence length #2: {}", pseu.sentlen);
+    off.words(s1_token);
+    pseu.words(s2_token);
+    off.sents(s1_sent);
+    pseu.sents(s2_sent);
 
 }
